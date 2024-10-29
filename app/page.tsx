b8d9276b062332ec5aa3bcc756f81864b4197e86
Mateus-Pasqueta <Mateuspasquetap@outlook.com>
2024-10-29 20:33:54 -0300

    feat: add sidebar and header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Header from "./components/header";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Mateus!</h2>
+        <p>Segunda-feira, 28 de outubro.</p>
+      </div>
     </div>
   );
 }
